a43a5865317a3f8841ec255fb1baf2989017d868
Luis Romano <luisromano@gmail.com>
2024-09-10 17:42:42 -0300

    Descarga acta word

--- solicitudes/urls.py
+++ solicitudes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SolicitudCreateView, SolicitudEnviadaView, SolicitudListView, SolicitudDeleteView, SolicitudDetailView
+from .views import SolicitudCreateView, SolicitudEnviadaView, SolicitudListView, SolicitudDeleteView, SolicitudDetailView, GenerarDocumentoView, DescargarDocumentoView
 from django.views.generic.base import TemplateView  # Importa TemplateView
 
 app_name = 'solicitudes'
@@ -9,5 +9,7 @@ urlpatterns = [
     path('solicitud-enviada/', SolicitudEnviadaView.as_view(), name='solicitud_enviada'),
     path('solicitudes/', SolicitudListView.as_view(), name='solicitud-list'),
     path('solicitudes/<int:pk>/', SolicitudDetailView.as_view(), name='solicitud_detail'),    
-    path('solicitudes/<int:pk>/borrar/', SolicitudDeleteView.as_view(), name='solicitud_delete'),    
+    path('solicitudes/<int:pk>/borrar/', SolicitudDeleteView.as_view(), name='solicitud_delete'),
+    path('generar_documento/<int:pk>/', GenerarDocumentoView.as_view(), name='generar_documento'),
+    path('descargar_documento/<str:file_name>/', DescargarDocumentoView.as_view(), name='descargar_documento'),    
 ]
--- solicitudes/views.py
+++ solicitudes/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
-from django.views.generic import (DetailView, DeleteView, ListView, TemplateView)
+from django.views.generic import (DetailView, DeleteView, ListView, TemplateView, View)
 from django.views.generic.edit import FormView
 from .models import Solicitud
 from .forms import SolicitudForm
+from docxtpl import DocxTemplate
+import uuid
+import os
+from django.core.files.storage import default_storage
+from django.utils import timezone
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
 class SolicitudCreateView(FormView):    
     form_class = SolicitudForm
@@ -36,3 +43,57 @@ class SolicitudDeleteView(LoginRequiredMixin, DeleteView):
 class SolicitudDetailView(LoginRequiredMixin, DetailView):
     model = Solicitud
     template_name = 'solicitudes/solicitud_detail.html'
+
+# Generacion de docx
+class GenerarDocumentoView(DetailView):
+    model = Solicitud
+    template_name = 'solicitudes/generar_documento.html'  # Una plantilla opcional si quieres mostrar algo en pantalla
+
+    def get(self, request, *args, **kwargs):
+        solicitud = self.get_object()
+
+        # Cargar la plantilla del archivo .docx
+        template = DocxTemplate('templates/ActaUso.docx')
+
+        # Definir el contexto con los datos del formulario
+        contexto = {            
+            'nombres_principal': solicitud.nombres_principal,
+            'apellidos_principal': solicitud.apellidos_principal,
+            'cargo_principal': solicitud.cargo_principal,
+            'institucion_principal': solicitud.institucion_principal,
+            'ciudad_principal': solicitud.ciudad_principal,
+            'pais_principal': solicitud.pais_principal,
+            'titulo_estudio': solicitud.titulo_estudio, 
+            'caracteristicas_poblacion': solicitud.caracteristicas_poblacion,
+            'dia_solicitud': solicitud.fecha_solicitud.day,
+            'mes_solicitud': solicitud.fecha_solicitud.month,
+            'anio_solicitud': solicitud.fecha_solicitud.year,          
+        }
+
+        # Rellenar la plantilla
+        template.render(contexto)
+
+        # Generar un nombre único para el archivo
+        file_name = f'documento_{uuid.uuid4()}.docx'
+
+        # Guardar el archivo temporalmente
+        file_path = os.path.join('media', 'temp_docs', file_name)
+        template.save(file_path)
+
+        # Redirigir a la vista de descarga con el archivo generado
+        return redirect('solicitudes:descargar_documento', file_name=file_name)
+
+class DescargarDocumentoView(View):
+    
+    def get(self, request, file_name):
+        file_path = os.path.join('media', 'temp_docs', file_name)
+
+        # Comprobar si el archivo existe
+        if default_storage.exists(file_path):
+            # Crear una respuesta HTTP con el archivo
+            with default_storage.open(file_path, 'rb') as docx_file:
+                response = HttpResponse(docx_file.read(), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+                response['Content-Disposition'] = f'attachment; filename={file_name}'
+                return response
+        else:
+            return HttpResponse("El enlace ha expirado o el archivo ya no está disponible.", status=404)
