dba911a3788c756d727811127d7fb5f386c1cd0c
Luis Romano <luisromano@gmail.com>
2024-09-25 15:00:42 -0300

    Fix minor bugs

--- scripts/systemd_and_nginx.sh
+++ scripts/systemd_and_nginx.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-echo "export DJANGO_SETTINGS_MODULE=pcatia.settings.production" >> ~/.bashrc
-echo "export DJANGO_SECRET_KEY='my_secret_key'" >> ~/.bashrc
-echo "export DJANGO_DB_NAME='pcatia_db'" >> ~/.bashrc
-echo "export DJANGO_DB_USER='pcatia_db_user'" >> ~/.bashrc
-echo "export DJANGO_DB_PASSWORD='pg_password'" >> ~/.bashrc
-echo "export DJANGO_DB_HOST='pg_host'" >> ~/.bashrc
-echo "export DJANGO_DB_PORT='5432'" >> ~/.bashrc
-
-source ~/.bashrc
-
-echo "Variables de entorno agregadas y .bashrc recargado."
-
 cp systemd/gunicorn.service /etc/systemd/system/
 cp systemd/gunicorn.socket /etc/systemd/system/
 systemctl start gunicorn.socket
